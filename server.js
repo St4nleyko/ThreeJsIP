ae87d7f878cd3f67d6648a9356ce7218f9522090
St4nleyko <Stanleyko7799@Ggmail.com>
2021-11-24 15:12:32 +0000

    more or less functional initial commit

---
+++ server.js
@@ -0,0 +1,82 @@
+// const express = require('express');
+// const app = express();
+const http = require('http');
+const server = http.createServer();
+const io = require('socket.io')(server,
+  {
+    cors:{
+      origin:"*"
+    }
+  });
+const _USERS = [];
+//when connection is made we will give player random location and get it back
+class ConnectedUser{
+  constructor(socket){
+    //amount of players
+    this.id=_USERS.length;
+    //initial position 3 params
+    this.pos_ = [Math.random() * 20, 0, Math.random() * 20];
+    this.playerName = "placeholderName"
+    console.log("initial player " +this.playerName+ " at initial pos: "+this.pos_);
+    //received socket
+    this.socket_ = socket;
+    //print position
+    const playerListener = (par) => {
+      //shows all data
+      console.log(this.id+par);
+      //shows all data as well as datatype
+      // console.log(this.id+par);
+
+    }
+    this.socket_.on("playerName", playerListener);
+    //
+    //when recievin socket call back anonymous function with
+    //get and send position
+    this.socket_.on('pos',(d) =>  {
+      //change position
+      this.pos_=[...d];
+
+      this.SendEveryone();
+    });
+    //get and send playerName
+    this.socket_.on('playerName',(par) =>  {
+      //change position
+      this.playerName=[par];
+
+      this.SendEveryone();
+    });
+   //get and send msg
+    this.socket_.on('chat',(msge) => {
+      const msg = msge;
+      const player = this.playerName;
+      const playerId = this.id;
+      this.SendEveryoneInChat(msg,player,playerId);
+    })
+    this.SendEveryone();
+  }
+  SendEveryoneInChat(msg,playerName,playerId){
+    for (let i = 0; i < _USERS.length; i++) {
+      _USERS[i].socket_.emit('chat', msg,playerName,playerId);      
+    }
+  }
+
+  SendEveryone(){
+    // id and position of user
+    this.socket_.emit('pos', [this.id, this.pos_]);
+    // keeps track of users
+    for (let i = 0; i < _USERS.length; i++) { 
+      _USERS[i].socket_.emit('pos', [this.id, this.pos_]);
+      this.socket_.emit('pos',[_USERS[i].id, _USERS[i].pos_]);
+    }
+  }
+}
+
+//server sends info about players via socket received after connection
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  _USERS.push(new ConnectedUser(socket))
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
