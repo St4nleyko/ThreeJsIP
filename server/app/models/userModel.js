68456db40ee5ab5c339f3a31520537b1e62b3174
St4nleyko <Stanleyko7799@Ggmail.com>
2021-12-28 17:23:20 +0100

    backend auth semiworking

---
+++ server/app/models/userModel.js
@@ -0,0 +1,29 @@
+// const bcrypt = require("bcrypt");
+
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    id: {
+      type: Sequelize.INTEGER,
+      unique: true,
+      primaryKey: true
+
+    },
+    character_id: {
+      type: Sequelize.INTEGER
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+      },
+    password: {
+        type: Sequelize.STRING
+      }
+    });
+    return User;
+
+};
