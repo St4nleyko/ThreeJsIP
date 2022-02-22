3b1293eda43243333809286e4983cc2898f11400
St4nleyko <Stanleyko7799@Ggmail.com>
2022-02-22 15:28:38 +0000

    almost working ws - need to polish positioning

--- server.js
+++ server.js
@@ -16,14 +16,7 @@ app.use(cors(corsOptions));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({type: 'application/json', limit: '300mb', extended: false}));
-// app.use(bodyParserErrorHandler());
 
-// app.use(bodyParser.raw({ type: 'application/vnd.custom-type', limit: '200MB', extended: false }))
-// app.use(bodyParser.urlencoded({limit: '200MB', extended: false}));
-
-// app.use(express.json({type: 'application/json', limit: '300mb', extended: false})).use(express.urlencoded())
-// app.use(express.json({ limit: '500MB' }))
-// app.use(express.urlencoded({ limit: '150mb', extended: true, parameterLimit: 50000 }))
 
 // simple route
 app.get("/", (req, res) => {
@@ -40,6 +33,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 const db = require("./app/models");
+const { portal } = require('./app/models');
+const { spawn } = require('child_process');
 
 
 
@@ -57,69 +52,89 @@ const io = require('socket.io')(server,
       origin:"*"
     }
   });
-const _USERS = [];
-//when connection is made we will give player random location and get it back
-class ConnectedUser{
-  constructor(socket){
-    this.socket_ = socket;
-    this.id =socket.handshake.query.userId;
-    console.log(_USERS);
-    console.log(socket.id)
-    this.portalId= socket.handshake.query.portalId;
-    this.playerName= socket.handshake.query.username;
-    const keysHeldListener = (keys) => {
-      this.socket_.emit('keypressed',keys)
-    }
-     this.socket_.on("keysHeld", keysHeldListener);
-    
-    this.portalIds = _USERS.length;
-    //initial position 3 params
-    this.pos_ = [Math.random() * 20, 0, Math.random() * 20];
-    // this.socket_.emit("position",this.pos_);
-    // this.portalPost = [Math.random() * 20,0,40];
 
-    console.log(this.id+": "+this.playerName+ " at initial pos: "+this.pos_+" connected");
-    this.socket_.on('pos',(d) =>  {
-      this.pos_=[d];
-      this.SendEveryone();
+
+const _USERS = {};
+
+function handleConnection(socket){
+//initialization
+  let socket_ = socket;
+  let userId =socket_.handshake.query.userId;
+  let portalId= socket_.handshake.query.portalId;
+  let playerName= socket_.handshake.query.username;
+  socket.join("portal" +portalId);
+  console.log(playerName+ ' joined a portal '+portalId)
+  io.in("portal"+portalId).emit('chat', "joined a game",playerName,userId);
+
+  _USERS[socket.id] = socket;
+  socket.on('disconnect', function() {
+    console.log('user disconnected!'+socket.id);
+    socket.emit('removePlayer', _USERS[socket.id]);
+    delete _USERS[socket.id];
+    console.log(Object.entries(_USERS));
+ });
+  //spawn
+  // NOTE for meeting: upload real time position to all players, spawn on file open sometimes bugs out, 
+  Object.keys(_USERS).forEach(function(key) {
+    if(!(_USERS[key].data.position)){
+      let initialPos = [Math.random() * 20, 0, Math.random() * 20];
+      initialPos = [...initialPos];
+      _USERS[key].data.position = initialPos;
+      console.log('initial pos: '+key, _USERS[key].data);
+      console.log(userId,playerName,initialPos,portalId)
+
+    }    
+  });
+  Object.keys(_USERS).forEach(function(i) {
+    console.log('    sending position to client'+_USERS[i].data.position)
+    io.in("portal"+portalId).emit('position', [userId, playerName, _USERS[i].data.position,portalId]);
+  });
+  //end initilazation
+
+
+
+  //sort of a switch - chat - is a event name and can work as "case" in switch 
+  // chat
+  socket_.on('chat',(msg) => {
+    handleMessages(socket_,msg,playerName,userId,portalId);
+  })
+  
+  //movement and position
+  const keysHeldListener = (keys) => {
+    socket_.emit('keypressed',keys)
+  }
+  socket_.on("keysHeld", keysHeldListener);
+  
+  socket_.on('position',(d) =>  {
+    let pos_=[...d];
+    socket.data.position =pos_;
+    console.log('pos_'+pos_)
+    Object.keys(_USERS).forEach(function(i) {
+      console.log('new position: '+i, _USERS[i].data.position);
     });
-   //get and send msg
-    this.socket_.on('chat',(msge) => {
-      const msg = msge;
-      const player = this.playerName;
-      const playerId = this.id;
-      
-      this.SendEveryoneInChat(msg,player,playerId,this.portalIds);
-    })
-    this.SendEveryone();
-  }
-  SendEveryoneInChat(msg,playerName,playerId,portalIds){
-    for (let i = 0; i < _USERS.length; i++) {
-      _USERS[i].socket_.emit('chat', msg,playerName,playerId,portalIds);      
-    }
-  }
-
-  SendEveryone(){
-    // id and position of user
-    this.socket_.emit('pos', [this.id, this.playerName, this.pos_, this.portalId]);
-    // keeps track of users
-    for (let i = 0; i < _USERS.length; i++) { 
-      _USERS[i].socket_.emit('pos', [this.id, this.playerName, this.pos_, this.portalId ]);
-      this.socket_.emit('pos',[_USERS[i].id, _USERS[i].playerName, _USERS[i].pos_, _USERS[i].portalId]);
-    }
-  }
+    handleMovement(socket ,socket.data.position,playerName,userId,portalId);
+  });
+  
 }
+
+
+function handleMessages(socket,msg,playerName,userId,portalId){
+    io.in("portal"+portalId).emit('chat', msg,playerName,userId);      
+}
+
+function handleMovement(socket,pos,playerName,userId,portalId){
+  console.log(pos,playerName,userId,portalId)
+  socket.to("portal"+portalId).emit('position', [userId, playerName, pos,portalId]);
+}
+
 //server sends info about players via socket received after connection
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  _USERS.push(new ConnectedUser(socket))
+  if(socket.handshake.query.userId){
+    handleConnection(socket);
+  }
 });
 
-// io.on('setuser',funkcn)
-// function funkcn(){
-//     console.log('asd')
-
-// }
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
